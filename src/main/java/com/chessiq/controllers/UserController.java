971dbbcea8faf55d2337be6edc0da431a9f89f73
DeltaLambda77 <danlozano77@gmail.com>
2025-04-11 19:33:08 -0400

    basic file strucutre + user models and controllers

---
+++ src/main/java/com/chessiq/controllers/UserController.java
@@ -0,0 +1,26 @@
+package com.chessiq.controller;
+
+import com.chessiq.dto.UserDTO;
+import com.chessiq.model.User;
+import com.chessiq.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@RequestBody UserDTO userDTO) {
+        try {
+            User createdUser = userService.createUser(userDTO);
+            return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+        }
+    }
+}
---
+++ src/main/java/com/chessiq/dto/UserDTO.java
@@ -0,0 +1,9 @@
+package com.chessiq.dto;
+
+public class UserDTO {
+    private String username;
+    private String email;
+    private String password;
+
+    // Getters and Setters
+}
